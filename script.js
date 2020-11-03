6aff1be982d358d888a65017a9fefd4a9de1fdbc
Asnise_Git <55057145+mastere00@users.noreply.github.com>
2020-11-03 23:58:31 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function cansel_win(idtag ,download)
+function cansel_win(idtag ,name,content,img_1,download)
 {
 	var win_alert = document.getElementById(idtag);
 	if (win_alert.style.display === "block")
@@ -8,5 +8,9 @@ function cansel_win(idtag ,download)
 	else
 	{
 		win_alert.style.display = "block";
-	}	
+	}
+		document.getElementById("img_1").src = img_1;
+		document.getElementById("title_name").innerHTML = name;
+		document.getElementById("content_mod").innerHTML = content;
+		document.getElementById("download_bt").href = download;
 };
